30e28d27b20c7a4170c07c4cec3d3e0295b26a16
Victor Lucas Gomes da Silva <167445059+Victor-Viicell@users.noreply.github.com>
2024-10-01 18:59:58 -0300

    modified:   bin/App.class 	modified:   src/App.java

--- src/App.java
+++ src/App.java
@@ -1,7 +1,12 @@
 
+import java.awt.*;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Runnable runner = () -> {
+
+        };
+        EventQueue.invokeLater(runner);
     }
 }
