c93a08a5467fed0aa6575ee518e166abddc5953d
Victor Lucas Gomes da Silva <167445059+Victor-Viicell@users.noreply.github.com>
2024-10-15 17:14:39 -0300

    a
    a

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -15,7 +17,10 @@ public class App extends Application {
     @Override
     public void start(@SuppressWarnings("exports") Stage stage) throws IOException {
 
-        scene = new Scene(root);
+        scene = new Scene(root, Color.BLACK);
+        Image icon = new Image(getClass().getResourceAsStream("icon.png"));
+        stage.getIcons().add(icon);
+        stage.setTitle("LotoSorte");
         stage.setScene(scene);
         stage.show();
     }
